cea2c861ac099c451fe88e6cb15d570247826d6f
ChanManChan <ngopal253@gmail.com>
2021-01-01 19:13:26 +0530

    users ms-2

--- PhotoAppApiUsers/src/main/java/com/u4/chan/photoapp/api/users/data/UsersRepository.java
+++ PhotoAppApiUsers/src/main/java/com/u4/chan/photoapp/api/users/data/UsersRepository.java
@@ -3,4 +3,6 @@ package com.u4.chan.photoapp.api.users.data;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UsersRepository extends CrudRepository<UserEntity, Long> {
+    // added findByEmail method because of loadUserByUsername method inside UsersServiceImpl
+    UserEntity findByEmail(String email);
 }
--- PhotoAppApiUsers/src/main/java/com/u4/chan/photoapp/api/users/service/UsersService.java
+++ PhotoAppApiUsers/src/main/java/com/u4/chan/photoapp/api/users/service/UsersService.java
@@ -1,7 +1,10 @@
 package com.u4.chan.photoapp.api.users.service;
 
 import com.u4.chan.photoapp.api.users.shared.UserDto;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UsersService {
+// extending UserDetailsService because of configure method inside WebSecurity
+public interface UsersService extends UserDetailsService {
     UserDto createUser(UserDto userDetails);
+    UserDto getUserDetailsByEmail(String email); // <- used inside AuthenticationFilter successfulAuthentication method
 }
